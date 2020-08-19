141cd8c57590433081176213fd4b25054eb5d3d8
Iamritwik783 <ritwik783@gmail.com>
2020-08-19 16:19:29 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My awesome react app @home.
+          My awesome react app @facebook.
         </p>
 
       </header>
