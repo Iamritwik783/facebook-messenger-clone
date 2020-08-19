792ab0993a90121f67202d0d46b2bb0fb804120f
Iamritwik783 <ritwik783@gmail.com>
2020-08-19 16:24:13 +0530

    initial setup complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My awesome react app @facebook.
-        </p>
+    <div>
 
-      </header>
     </div>
   );
 }
