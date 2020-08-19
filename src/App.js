fa73ffdb9e6bb73b3ba66c1a6df3dcf958782d87
Iamritwik783 <ritwik783@gmail.com>
2020-08-19 21:43:00 +0530

    structure without message ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import './Input.css'
 
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  //console.log(input);
+
+  const [messages, setMessages] = useState([{user: "Sonu", message: "Hello"}, {user: "Khata", message: "Hi"}]);
+
+  const [username, setUsername] = useState('');
+
+  useEffect(()=>{
+    setUsername(prompt("Please enter your name: "));
+  }, [])
+  
+  const sendMessage = (event) => {
+    //to stop the form, from reloading
+    event.preventDefault();
+
+    setMessages([...messages, {user: username, message: input}]);
+  }
+
+
+
   return (
     <div >
 
       <div className="input__tab">
         <form>
+          
           <input className = "input__bar"
+            value={input}
+            onChange = {event => setInput(event.target.value)}
             placeholder="Enter your message...."
           />
-          <Button variant="outlined" color="primary">
-            Primary
+
+
+          <Button 
+            variant="outlined" 
+            disabled = {!input}
+            color="primary"
+            type = "submit"
+            onClick = {sendMessage}
+            >Send Message
           </Button>
+          
         </form>
 
       </div>
